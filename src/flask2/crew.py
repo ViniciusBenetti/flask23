6c1ec040dd8c91ac7c125679fbbaa4a3acdb6fcc
dicasdodepatri <dicasdodepatri@gmail.com>
2025-06-11 11:16:58 -0300

    rota criada

--- src/flask2/crew.py
+++ src/flask2/crew.py
@@ -5,10 +5,16 @@ from crewai.project import CrewBase, agent, crew, task
 class CybersecurityProject():
     """Cybersecurity and Content Creation crew"""
 
+
+    def __init__(self, llm=None):
+        self.llm = llm  # Recebe o LLM configurado
+
+
     @agent
     def cybersecurity_expert(self) -> Agent:
         return Agent(
-            config=self.agents_config['cybersecurity_expert'],
+            config=self.agents_config['cybersecurity_expert'], 
+            llm=self.llm,
             verbose=True
         )
 
@@ -22,7 +28,7 @@ class CybersecurityProject():
     @crew
     def crew(self) -> Crew:
         """Creates the Cybersecurity Project crew"""
-        return Crew(
+        return Crew(  
             agents=[self.cybersecurity_expert()],
             tasks=[self.scam_check_task()],
             process=Process.sequential,
---
+++ src/flask2/main.py
@@ -0,0 +1,62 @@
+import sys 
+import os
+import warnings
+from datetime import datetime
+from crewai import LLM
+from crew import CybersecurityProject
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+os.environ["GROQ_API_KEY"] = "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"  
+
+
+llm = LLM(
+    model="groq/gemma2-9b-it",  
+    api_key=os.environ.get("GROQ_API_KEY")
+)
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+def run(msg=None):
+    """Executa as funções sequencialmente, uma por vez"""
+    try:
+        scam_result = check_scam(msg)
+        print(scam_result)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+def check_scam(user_story: str):
+    """Executa apenas a tarefa de verificação de golpe"""
+    inputs = {
+        'topic': user_story,
+        'current_year': str(datetime.now().year),
+        'task_type': 'check_scam'
+    }
+    
+    try:
+        result = CybersecurityProject(llm=llm).crew().kickoff(inputs=inputs)
+        return result
+    except Exception as e:
+        raise Exception(f"An error occurred while checking for scam: {e}")
+    
+
+@app.route('/',methods=['POST'])
+def checkScam():
+    if request.method == 'POST':
+        try:
+            input = request.get_json()
+            mensagem = input.get('mensagem')
+        
+            result = run(mensagem)
+            print('sucesso')
+            return jsonify({"resposta":result}),200
+        except Exception as e:
+            print(e)
+            return jsonify({"resposta":"erro interno"}),500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)

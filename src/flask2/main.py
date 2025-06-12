770a7857abd3765e86861832d3f0405cdbd3bae3
dicasdodepatri <dicasdodepatri@gmail.com>
2025-06-12 10:54:23 -0300

    tunnel

--- src/flask2/main.py
+++ src/flask2/main.py
@@ -1,12 +1,14 @@
-import sys 
 import os
+import sys
 import warnings
 from datetime import datetime
 from crewai import LLM
 from crew import CybersecurityProject
 import os
+import signal
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pyngrok import ngrok, conf
 
 app = Flask(__name__)
 CORS(app)
@@ -44,6 +46,7 @@ def check_scam(user_story: str):
         raise Exception(f"An error occurred while checking for scam: {e}")
     
 
+
 @app.route('/',methods=['POST'])
 def checkScam():
     if request.method == 'POST':
@@ -57,6 +60,23 @@ def checkScam():
         except Exception as e:
             print(e)
             return jsonify({"resposta":"erro interno"}),500
+        
+def start_ngrok():
+    conf.get_default().auth_token = "2yPVGVqqzKuw7ix1b06J6ttR8GT_2FAoFo8pFgYmQP48n5ea"
+    tunnel = ngrok.connect(5000, "http")
+    print(f"URL pública do ngrok: {tunnel.public_url}")
+    return tunnel
+
+def signal_handler(sig, frame):
+    ngrok.disconnect(ngrok.get_tunnels()[0].public_url)
+    print("\nNgrok e Flask encerrados.")
+    sys.exit(0)
+
+
 
 if __name__ == "__main__":
+    # Iniciar o ngrok
+    tunnel = start_ngrok()
+    # Capturar Ctrl+C para encerrar graceful
+    signal.signal(signal.SIGINT, signal_handler)
     app.run(host='0.0.0.0',port=5000,debug=True)
